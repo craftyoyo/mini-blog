7ecea61c794ad2797237ac67e34bdad937ce397f
tomual <tomual@users.noreply.github.com>
2018-07-05 19:35:46 -0500

    Single blog mode

--- application/config/config.php
+++ application/config/config.php
@@ -521,3 +521,6 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+$config['single_blog_mode'] = TRUE;
+$config['single_blog_username'] = 'username';
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,11 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		if($this->config->item('single_blog_mode')) {
+			$blog = $this->blogs_model->get_blog_by_username($this->config->item('single_blog_username'));
+			$this->load->view('blog/home', compact('blog'));
+			return;
+		}
 		$this->load->view('home');
 	}
 }
